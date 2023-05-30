dd81dd04b17968aa23c8af150b33c9f0d00158da
Grasix3 <134703570+Grasix3@users.noreply.github.com>
2023-05-30 16:58:42 +0200

    Update n_eignungsdiagnostik.js
    Fragenkatalog erweitert, Bilder zum interpretieren eingebaut.
    Änderungen an der Mail Funktion.

--- n_eignungsdiagnostik.js
+++ n_eignungsdiagnostik.js
@@ -1,22 +1,22 @@
 const questions = [
   {
-    question: 'Was ist das im Bild gezeigte Tier? <img src="./conti_logo_transparent.png" alt="Tier" />',
+    question: 'Interpretieren Sie folgenden Code, welche Ausgabe erhält man? <img class="question-image" src="file:///C:/Users/uic88596/Documents/Conti%20-%20Unterlagen/Neuer%20Ordner/eign/Codeschnippsel1.png" alt="Codebeispiel" />',
     answers: [
-      { text: "Katze", correct: false },
-      { text: "Löwe", correct: false },
-      { text: "Elefant", correct: true },
+      { text: "Code ist fehlerhaft, kein Rückgabewert", correct: false },
+      { text: "15", correct: true },
+      { text: "10", correct: false },
     ],
-    correctAnswer: 2,
+    correctAnswer: 1,
     tag: "Analyse"
   },
   {
     question: "Was ist ein API (Application Programming Interface)?",
     answers: [
       { text: "Eine Programmiersprache", correct: false },
-      { text: "Ein Vertrag zwischen Softwaresystemen, der festlegt, wie sie Informationen austauschen", correct: true },
       { text: "Ein Betriebssystem", correct: false },
+      { text: "Ein Vertrag zwischen Softwaresystemen, der festlegt, wie sie Informationen austauschen", correct: true },
     ],
-    correctAnswer: 1,
+    correctAnswer: 2,
     tag: "allgemeine Informatik"
   },
   {
@@ -27,16 +27,16 @@ const questions = [
       { text: "Einen Maschinensprachen-Code in Hochsprache umzuwandeln", correct: false },
     ],
     correctAnswer: 0,
-    tag: "C"
+    tag: "allgemeine Informatik"
   },
   {
     question: "Welche der folgenden Aussagen ist eine gültige Definition für eine Funktion?",
     answers: [
-      { text: "Eine Sammlung von Anweisungen, die eine bestimmte Aufgabe ausführen", correct: true },
       { text: "Ein Speicherbereich für Daten", correct: false },
+      { text: "Eine Sammlung von Anweisungen, die eine bestimmte Aufgabe ausführen", correct: true },
       { text: "Eine Schleife, die wiederholt wird", correct: false },
     ],
-    correctAnswer: 0,
+    correctAnswer: 1,
     tag: "allgemeine Informatik"
   },
   {
@@ -50,6 +50,16 @@ const questions = [
     tag: "allgemeine Informatik"
   },
   {
+    question: 'Interpretieren Sie folgenden Code, welche Ausgabe erhält man? <img class="question-image" src="file:///C:/Users/uic88596/Documents/Conti%20-%20Unterlagen/Neuer%20Ordner/eign/Codeschnippsel2.png" alt="Codebeispiel" />',
+    answers: [
+      { text: "0", correct: false },
+      { text: "%d\n, *p + 2", correct: false },
+      { text: "7", correct: true },
+    ],
+    correctAnswer: 2,
+    tag: "Analyse"
+  },
+  {
     question: "Was ist eine rekursive Funktion?",
     answers: [
       { text: "Eine Funktion, die sich selbst aufruft", correct: true },
@@ -57,7 +67,7 @@ const questions = [
       { text: "Eine Funktion, die auf einen Webserver zugreift", correct: false },
     ],
     correctAnswer: 0,
-    tag: "Python"
+    tag: "allgemeine Informatik"
   },
   {
     question: "Was verstehen Sie unter SQL?",
@@ -80,11 +90,11 @@ const questions = [
     tag: "IT-Sec"
   },
   {
-    question: "Wofür steht die Abkürzung SEO?",
+    question: "Wofür steht die Abkürzung SEO im Zusammenhang mit Informatik?",
     answers: [
       { text: "Search Engine Optimization", correct: true },
       { text: "Senior Executive Officer", correct: false },
-      { text: "Search Expo", correct: false },
+      { text: "Search Export Operator", correct: false },
     ],
     correctAnswer: 0,
     tag: "Reports"
@@ -97,14 +107,14 @@ const questions = [
       { text: "16", correct: false },
     ],
     correctAnswer: 0,
-    tag: "Elektrotechnik"
+    tag: "Grundlagen Informatik"
   },
   {
     question: "Welches der folgenden Begriffe stehen für eine Programmiersprache?",
     answers: [
-      { text: "C++", correct: true },
+      { text: "C", correct: true },
       { text: "Jawa", correct: false },
-      { text: "PowerrBI", correct: false },
+      { text: "PowerBI", correct: false },
     ],
     correctAnswer: 0,
     tag: "C"
@@ -170,7 +180,284 @@ const questions = [
     ],
     correctAnswer: 2,
     tag: "Controlling"
-  }  
+  },
+  
+  // Analyse
+  {
+    question: "Was versteht man unter dem Begriff 'Regression' in der Datenanalyse?",
+    answers: [
+      { text: "Ein statistisches Verfahren zur Vorhersage von Werten basierend auf bekannten Datenpunkten", correct: true },
+      { text: "Eine Methode zur Clusteranalyse von Daten", correct: false },
+      { text: "Die Auswertung von Datenbankabfragen", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "Analyse"
+  },
+  {
+    question: "Was ist der Zweck einer Korrelationsanalyse in der Datenanalyse?",
+    answers: [
+      { text: "Den kausalen Zusammenhang zwischen zwei Variablen zu bestimmen", correct: false },
+      { text: "Die Verteilung von Daten zu analysieren", correct: false },
+      { text: "Den Grad der Beziehung zwischen zwei Variablen zu messen", correct: true },
+    ],
+    correctAnswer: 2,
+    tag: "Analyse"
+  },
+  
+  // allgemeine Informatik
+  {
+    question: "Was ist ein Stack Overflow?",
+    answers: [
+      { text: "Ein Fehler im Speichermanagement eines Programms", correct: true },
+      { text: "Eine Website für Fragen und Antworten zur Programmierung", correct: false },
+      { text: "Ein Sicherheitsangriff auf Netzwerke", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "allgemeine Informatik"
+  },
+  {
+    question: "Was ist der Unterschied zwischen einem nativen und einem interpretierten Programm?",
+    answers: [
+      { text: "Ein nativer Code wird direkt auf der Hardware ausgeführt, während ein interpretierter Code schrittweise von einem Interpreter ausgeführt wird", correct: true },
+      { text: "Ein nativer Code ist schneller als ein interpretierter Code", correct: false },
+      { text: "Ein interpretierter Code wird in einer bestimmten Programmiersprache geschrieben, während ein nativer Code hardwarenah ist", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "allgemeine Informatik"
+  },
+  
+  // Datenbanken
+  {
+    question: "Was ist ein Datenbankindex und wie kann er die Abfrageleistung verbessern?",
+    answers: [
+      { text: "Ein Datenbankindex ist eine Kopie der gesamten Datenbank, die für schnelle Abfragen verwendet wird", correct: false },
+      { text: "Ein Datenbankindex ist eine Datenstruktur, die den Zugriff auf bestimmte Daten in einer Datenbank beschleunigt", correct: true },
+      { text: "Ein Datenbankindex ist ein zusätzlicher Speicherplatz für Sicherungszwecke", correct: false },
+    ],
+    correctAnswer: 1,
+    tag: "Datenbanken"
+  },
+  {
+    question: "Was ist der Unterschied zwischen einem INNER JOIN und einem OUTER JOIN in SQL?",
+    answers: [
+      { text: "Ein INNER JOIN gibt nur die übereinstimmenden Zeilen aus, während ein OUTER JOIN auch nicht übereinstimmende Zeilen ausgibt", correct: true },
+      { text: "Ein INNER JOIN kann nur zwischen zwei Tabellen durchgeführt werden, während ein OUTER JOIN mehrere Tabellen unterstützt", correct: false },
+      { text: "Ein INNER JOIN gibt alle Zeilen aus, während ein OUTER JOIN nur die übereinstimmenden Zeilen ausgibt", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "Datenbanken"
+  },
+  
+  // IT-Sec
+  {
+    question: "Was ist ein Firewall und welche Aufgabe hat sie in der Informationssicherheit?",
+    answers: [
+      { text: "Eine Firewall ist eine Software, die den Zugriff auf das Internet regelt und unerwünschte Netzwerkverbindungen blockiert", correct: true },
+      { text: "Eine Firewall ist ein physisches Gerät, das vor Überhitzung schützt und Brände verhindert", correct: false },
+      { text: "Eine Firewall ist eine Sicherheitsmaßnahme für den physischen Zugang zu einem Gebäude", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "IT-Sec"
+  },
+  {
+    question: "Was ist eine Denial-of-Service (DoS)-Attacke und wie funktioniert sie?",
+    answers: [
+      { text: "Eine DoS-Attacke ist ein Versuch, Informationen aus einer Datenbank zu extrahieren", correct: false },
+      { text: "Eine DoS-Attacke ist ein Angriff, der darauf abzielt, ein Netzwerk oder einen Dienst durch Überlastung lahmzulegen", correct: true },
+      { text: "Eine DoS-Attacke ist ein Versuch, die Benutzeranmeldung eines Systems zu umgehen", correct: false },
+    ],
+    correctAnswer: 1,
+    tag: "IT-Sec"
+  },
+  
+  // Reports
+  {
+    question: "Was ist ein Dashboard in einem Business Intelligence-System?",
+    answers: [
+      { text: "Ein Dashboard ist ein Bericht, der automatisch generiert wird und die wichtigsten Geschäftskennzahlen visualisiert", correct: true },
+      { text: "Ein Dashboard ist ein Tool zur Erstellung von Tabellen und Diagrammen in einem Bericht", correct: false },
+      { text: "Ein Dashboard ist eine Methode zum Drucken von Berichten auf Papier", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "Reports"
+  },
+  {
+    question: "Was versteht man unter dem Begriff 'Drill-Through' in einem Bericht?",
+    answers: [
+      { text: "Drill-Through ermöglicht es Benutzern, von einem summarischen Bericht zu detaillierten Informationen zu wechseln", correct: true },
+      { text: "Drill-Through ist eine Methode zur Datenverschlüsselung in einem Bericht", correct: false },
+      { text: "Drill-Through ist ein Tool zur Zusammenführung von Daten aus verschiedenen Datenquellen", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "Reports"
+  },
+  
+  // Grundlagen Informatik
+  {
+    question: "Was ist ein Algorithmus?",
+    answers: [
+      { text: "Ein Algorithmus ist eine Programmiersprache", correct: false },
+      { text: "Ein Algorithmus ist eine Schritt-für-Schritt-Anleitung zur Lösung eines Problems", correct: true },
+      { text: "Ein Algorithmus ist ein Computermodell für die Simulation realer Phänomene", correct: false },
+    ],
+    correctAnswer: 1,
+    tag: "Grundlagen Informatik"
+  },
+  {
+    question: "Was ist der Unterschied zwischen einer While-Schleife und einer For-Schleife in der Programmierung?",
+    answers: [
+      { text: "While-Schleife: Unbestimmte Anzahl von Iterationen. For-Schleife: Feste Anzahl von Iterationen.", correct: false },
+      { text: "While-Schleife: Vorhersehbare Schleife. For-Schleife: Unbestimmte Anzahl von Iterationen.", correct: false },
+      { text: "While-Schleife: Unbekannte Anzahl von Iterationen. For-Schleife: Bekannte Anzahl von Iterationen.", correct: true },
+    ],
+    correctAnswer: 2,
+    tag: "Grundlagen Informatik"
+  },
+  
+  // C
+  {
+    question: "Was ist der Unterschied zwischen einer Funktion und einer Prozedur in der Programmierung?",
+    answers: [
+      { text: "Eine Funktion gibt einen Rückgabewert zurück, während eine Prozedur keinen Rückgabewert hat", correct: true },
+      { text: "Eine Funktion ist in C geschrieben, während eine Prozedur in C++ geschrieben ist", correct: false },
+      { text: "Eine Funktion kann nur in Main-Funktionen verwendet werden, während eine Prozedur in beliebigen Funktionen verwendet werden kann", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "C"
+  },
+  {
+    question: "Was ist der Unterschied zwischen dem Präprozessor und dem Compiler in C?",
+    answers: [
+      { text: "Der Präprozessor übersetzt den C-Code in Maschinencode, während der Compiler Vorprozessordirektiven wie #include verarbeitet", correct: false },
+      { text: "Der Präprozessor fügt den Header-Code in den C-Code ein, während der Compiler den C-Code in Maschinencode übersetzt", correct: true },
+      { text: "Der Präprozessor kompiliert den C-Code in Assemblersprache, während der Compiler den Assemblercode in Maschinencode übersetzt", correct: false },
+    ],
+    correctAnswer: 1,
+    tag: "C"
+  },
+  
+  // logisches Denken
+  {
+    question: "Setzen Sie die Zahlenreihe fort: 5-10-20-40-?",
+    answers: [
+      { text: "80", correct: true },
+      { text: "60", correct: false },
+      { text: "100", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "logisches Denken"
+  },
+  {
+    question: "Welche Zahl gehört nicht zu der folgenden Reihe? 2-5-10-17-28",
+    answers: [
+      { text: "10", correct: false },
+      { text: "17", correct: false },
+      { text: "28", correct: true },
+    ],
+    correctAnswer: 2,
+    tag: "logisches Denken"
+  },
+  
+  // KI
+  {
+    question: "Was ist der Unterschied zwischen maschinellem Lernen und tiefem Lernen (Deep Learning)?",
+    answers: [
+      { text: "Maschinelles Lernen verwendet spezifische Algorithmen, während tiefes Lernen neuronale Netzwerke mit mehreren Schichten verwendet", correct: true },
+      { text: "Maschinelles Lernen ist ein übergeordneter Begriff für KI, während tiefes Lernen spezifisch auf neuronale Netzwerke verweist", correct: false },
+      { text: "Maschinelles Lernen verwendet überwachte Lernmethoden, während tiefes Lernen unsupervised Lernmethoden verwendet", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "KI"
+  },
+  {
+    question: "Was versteht man unter dem Begriff 'Deep Learning'?",
+    answers: [
+      { text: "Ein Algorithmus, der es Computern ermöglicht, menschenähnliches Denken und Verstehen zu erreichen", correct: false },
+      { text: "Ein Bereich der KI, der sich auf die Verarbeitung großer Mengen von Daten durch neuronale Netzwerke konzentriert", correct: true },
+      { text: "Ein Prozess, bei dem KI-Systeme aus Erfahrungen lernen und ihre Leistung verbessern", correct: false },
+    ],
+    correctAnswer: 1,
+    tag: "KI"
+  },
+  
+  // Cloud
+  {
+    question: "Was versteht man unter dem Begriff 'Skalierbarkeit' in Bezug auf Cloud-Computing?",
+    answers: [
+      { text: "Skalierbarkeit bezieht sich auf die Möglichkeit, Ressourcen in der Cloud dynamisch anzupassen, um den aktuellen Bedarf zu erfüllen", correct: true },
+      { text: "Skalierbarkeit bezieht sich auf die Geschwindigkeit der Datenübertragung in der Cloud", correct: false },
+      { text: "Skalierbarkeit bezieht sich auf die Sicherheit von Daten in der Cloud", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "Cloud"
+  },
+  {
+    question: "Was ist ein Cloud-Service-Modell?",
+    answers: [
+      { text: "Ein Cloud-Service-Modell beschreibt die physische Infrastruktur einer Cloud-Umgebung", correct: false },
+      { text: "Ein Cloud-Service-Modell beschreibt die Verantwortlichkeiten und den Umfang der bereitgestellten Cloud-Dienste", correct: true },
+      { text: "Ein Cloud-Service-Modell beschreibt die Anwendungsentwicklung für die Cloud", correct: false },
+    ],
+    correctAnswer: 1,
+    tag: "Cloud"
+  },
+  
+  // Embedded Systems
+  {
+    question: "Was ist ein Mikrocontroller und wie unterscheidet er sich von einem Mikroprozessor?",
+    answers: [
+      { text: "Ein Mikrocontroller ist ein integrierter Schaltkreis, der einen Mikroprozessor, Speicher und Peripheriegeräte auf einem einzigen Chip kombiniert", correct: true },
+      { text: "Ein Mikrocontroller ist eine spezielle Software zur Steuerung von Mikroprozessoren", correct: false },
+      { text: "Ein Mikrocontroller ist ein kleinerer und leistungsstärkerer Mikroprozessor", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "Embedded Systems"
+  },
+  {
+    question: "Was ist ein Echtzeitbetriebssystem (RTOS) und welche Anwendungen haben RTOS?",
+    answers: [
+      { text: "Ein RTOS ist ein Betriebssystem für zeitkritische Anwendungen, z.B. Flugzeugsteuerungssysteme.", correct: true },
+      { text: "Ein RTOS ermöglicht Echtzeitkommunikation zwischen Computern.", correct: false },
+      { text: "Ein RTOS wird in eingebetteten Systemen verwendet, um die Anwendungsleistung zu steigern.", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "Embedded Systems"
+  },  
+  
+  // Controlling
+  {
+    question: "Beschreiben Sie den Prozess der Budgetierung in einem Unternehmen.",
+    answers: [
+      { text: "Planung und Festlegung finanzieller Ziele für einen bestimmten Zeitraum.", correct: true },
+      { text: "Aufteilung des Unternehmens in verschiedene Abteilungen und Bereiche.", correct: false },
+      { text: "Bewertung der Leistung von Mitarbeitern im Unternehmen.", correct: false },
+    ],
+    correctAnswer: 0,
+    tag: "Controlling"
+  },
+  {
+    question: "Was versteht man unter dem Begriff 'Kostenrechnung' und welche Arten von Kosten gibt es?",
+    answers: [
+      { text: "Analyse von Kundennachfrage und Preisbildung. Es gibt Einzelkosten, Gemeinkosten und Opportunitätskosten.", correct: false },
+      { text: "Ermittlung und Überwachung der Kosten im Unternehmen. Es gibt fixe, variable und gemischte Kosten.", correct: true },
+      { text: "Berechnung des Gewinns eines Unternehmens. Es gibt monetäre und nicht-monetäre Kosten.", correct: false },
+      
+    ],
+    correctAnswer: 1,
+    tag: "Controlling"
+  },
+  {
+    question: "Erläutern Sie den Unterschied zwischen internem und externem Controlling.",
+    answers: [
+      
+      { text: "Internes Controlling: Analyse und Optimierung interner Kommunikation. Externes Controlling: Verwaltung von Kundenbeziehungen.", correct: false },
+      { text: "Internes Controlling: Verwaltung und Wartung interner IT-Systeme. Externes Controlling: Nutzung externer IT-Dienstleistungen.", correct: false },
+      { text: "Internes Controlling: Überwachung und Steuerung von Unternehmensprozessen und -leistungen. Externes Controlling: Zusammenarbeit mit externen Partnern.", correct: true },
+    ],
+    correctAnswer: 2,
+    tag: "Controlling"
+  }
+  
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -338,7 +625,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const departmentRecommendations = analyzeTags();
     const recommendationText = departmentRecommendations.length > 0
       ? `Basierend auf ${firstName} ${lastName}'s Antworten empfehlen wir folgende Abteilung(en): ${departmentRecommendations.join(', ')}`
-      : `Leider haben wir keine spezifische Abteilungsempfehlung basierend auf ${firstName} ${lastName}'s Antworten.`;
+      : `Leider haben wir keine spezifische Abteilungsempfehlung basierend auf ${firstName} ${lastName}'s Antworten. HINWEIS: Das geschieht nur, wenn der Benutzer alles falsch beantwortet hat.`;
   
     resultContainer.innerHTML = `<p>${resultText}</p><p>${recommendationText}</p>`;
     resultContainer.style.display = 'block';
@@ -358,7 +645,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
   
     // E-Mail senden
-    emailjs.send('service_ghse5za', 'template_cad1o25', templateParams, 'UBoWE2WCtSjUpgpQK')
+    emailjs.send('service_weu0lsp', 'template_qv9theq', templateParams, 'JF73zWJMp6q5M8-dq')
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
     }, function(error) {
